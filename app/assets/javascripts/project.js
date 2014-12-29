62cecce270516387034a34ea0f6973d1b4074e1a
KingOfTheNerds32 <mlarner@alumni.usc.edu>
2014-12-28 22:28:48 -0800

    Implemented floatThead. TODO: Fix minor formatting issues.

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,12 +1,16 @@
-  var main = function(){
-    $('.table-section').click(function(){
+var main = function(){
+  $('.table-section').click(function(){
+    $(this).nextUntil('.table-section').slideToggle(0);
+  });
+  $('#expand_collapse').click(function(){
+    $('.table-section').each(function(){
       $(this).nextUntil('.table-section').slideToggle(0);
     });
-    $('#expand_collapse').click(function(){
-      $('.table-section').each(function(){
-        $(this).nextUntil('.table-section').slideToggle(0);
-      });
-    });
-  };
+  });
+};
 
-  $(document).ready(main);
+$(document).ready(main);
+
+$(function(){
+    $('.table').floatThead();
+});
